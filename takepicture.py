64e018620cbc1ad474f583df957b1ee42ef0de07
Michanix <michanix@protonmail.com>
2019-11-10 10:30:44 +0200

    cleaning

--- takepicture.py
+++ takepicture.py
@@ -16,7 +16,7 @@ class TakePicture:
         self.capture = cv2.VideoCapture(0)
         self.current_image = None  # current image from the camera
 
-        self.root = Toplevel()  # initialize root window
+        self.root = Toplevel()  # initialize second window
         self.root.title("Photo Booth")  # set window title
         # self.destructor function gets fired when the window is closed
         self.root.protocol('WM_DELETE_WINDOW', self.destructor)
@@ -24,10 +24,11 @@ class TakePicture:
         self.panel = ttk.Label(self.root)  # initialize image panel
         self.panel.grid(column=0, row=0, columnspan=2,
                         rowspan=1, padx=10, pady=10)
-        # create a button, that when pressed, will take the current frame and save it to file
+        # take picture button
         take_image_btn = ttk.Button(
             self.root, text="Cheese!", command=self.take_snapshot)
         take_image_btn.grid(column=0, row=2, pady=5)
+        # exit / cancel
         cancel_btn = ttk.Button(self.root, text="Cancel",
                                 command=self.destructor)
         cancel_btn.grid(column=1, row=2, pady=5)
@@ -55,18 +56,16 @@ class TakePicture:
         """ Take snapshot and save it to the file """
         sleep(1)  # add some effect to indicate that picture been taken
         image_name = 'face.jpg'  # construct filename
-        path_to_save = 'images/{}'.format(image_name)
+        path_to_save = 'images/{}'.format(image_name)  # where to save image
         self.current_image.save(path_to_save)  # save image as jpeg file
         print("[INFO] saving image: {}".format(image_name))
 
     def destructor(self):
-        """ Destroy the root object and release all resources """
+        """ Destroy the second window object and release all resources """
         print("[INFO] closing TakePicture...")
         self.root.destroy()
         self.capture.release()  # release web camera
         cv2.destroyAllWindows()  # it is not mandatory in this application
-
-# construct the argument parse and parse the arguments
 
 
 # start the app
