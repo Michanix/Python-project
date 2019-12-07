8eac1831950e8baa152c028f206e4eefc58857ce
Michanix <michanix@protonmail.com>
2019-12-07 17:01:24 +0200

    comments added

--- src/main.py
+++ src/main.py
@@ -14,47 +14,58 @@ class UI:
 
     def __init__(self):
         self.root = Tk()
-        self.root.title("Main programm")
-        self.root.geometry('480x300')
+        self.root.title("Folder security")
 
-        self.mainframe = ttk.Frame(self.root, padding="3 3 12 12")
+        # Main window
+        self.mainframe = ttk.Frame(
+            self.root, padding="4 4 12 12", width=200, height=200)
         self.mainframe.grid(column=0, row=0)
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
-
-        self.label = ttk.Label(self.mainframe, text='Choose folder:')
-        self.label.grid(column=1, row=1)
-        self.folder = ttk.Entry(self.mainframe, text='')
-        self.folder.grid(column=2, row=1)
+        # Labels
+        self.folder_label = ttk.Label(self.mainframe, text='Choose folder:')
+        self.test_label = ttk.Label(self.mainframe, text='Test your image before applying changes')
+        # Entries
+        self.folder = ttk.Entry(self.mainframe, width=90, text='')
+        # Buttons
         self.browse = ttk.Button(
-            self.mainframe, text='Browse...', command=self.get_folder)
-        self.browse.grid(column=3, row=1, padx=10, pady=10)
+            self.mainframe, text='Browse...', command=self.browse)
         # upload an image button
         # call method upload_existing_picture from controllers.py
         self.upload_image_btn = ttk.Button(
             self.mainframe, text='Use existing image', command=use_existing_image)
-        self.upload_image_btn.grid(column=1, row=2)
         # button to take picture
         # call method take_picture from takepicture.py
         self.take_pic_btn = ttk.Button(
             self.mainframe, text='Take picture', command=TakePicture)
-        self.take_pic_btn.grid(
-            column=1, row=3, padx=10, pady=10)
         # button to test, if image is satisfy
         # call unlock method from controllers.py
         self.test_btn = ttk.Button(
             self.mainframe, text='Test', command=unlock)
-        self.test_btn.grid(column=1, row=4)
         # exit button
         self.exit_btn = ttk.Button(
             self.mainframe, text='Exit', command=self.close_program)
-        self.exit_btn.grid(column=1, row=5, padx=10, pady=10)
+        # Grigds
+        # folder grids
+        self.folder_label.grid(column=1, row=1)
+        self.folder.grid(column=2, row=1)
+        # browser button grid
+        self.browse.grid(column=3, row=1, padx=10, pady=10)
+        # use existing image grid
+        self.upload_image_btn.grid(column=2, row=2)
+        # take picture grid
+        self.take_pic_btn.grid(column=2, row=3, padx=10, pady=10)
+        # test button grid
+        self.test_label.grid(column=1, row=4)
+        self.test_btn.grid(column=2, row=4)
+        # exit button
+        self.exit_btn.grid(column=2, row=5, padx=10, pady=10)
 
     def close_program(self):
         self.root.destroy()
         print('Terminated.')
 
-    def get_folder(self):
+    def browse(self):
         user_desk = '/home/{}/Desktop'.format(getuser())
         self.browse = filedialog.askdirectory(
             initialdir=user_desk, title='Select folder')
