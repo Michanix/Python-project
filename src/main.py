b950be0ba339b03bac312359a53bec8000bb25b1
Michanix <michanix@protonmail.com>
2019-12-08 20:30:09 +0200

    remove paths from file finished

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # import only neccessary stuff
 import os
-from getpass import getuser # to username that currently logged in
+from getpass import getuser  # to username that currently logged in
 from subprocess import call
 
 from tkinter import Tk
@@ -14,38 +14,63 @@ from findandrecognize import find_and_recognize
 
 # set of directories that already secured
 
+def get_paths_from_dir():
+    with open('paths.txt', 'r') as f:
+        data = f.readlines()
+    return data
+
+def write_path_to_file(path):
+    # writing paths to file
+    path_to_save = os.path.join(os.getcwd(), 'paths.txt')
+    with open(path_to_save, 'a+') as f:
+        f.write(path + '\n')
+
+def remove_paths(path):
+    data = get_paths_from_dir()
+    data = [i.strip('\n') for i in data]
+    data.remove(path)
+    with open('paths.txt', 'w+') as f:
+        for p in data:
+            f.write(p + '\n')
+
 class UI:
 
     def __init__(self):
         self.root = Tk()
         self.root.title("Folder security")
         self.path_to_image = []
-        self.set_of_paths = set()
-        
         # Main window
         self.mainframe = ttk.Frame(
             self.root, padding="4 4 12 12", width=480, height=320)
         self.mainframe.pack()
-        #self.root.columnconfigure(0, weight=1)
-        #self.root.rowconfigure(0, weight=1)
         # menu bar
         menubar = Menu(self.root)
         menubar.add_command(label='Take picture', command=TakePicture)
-        menubar.add_command(label='Use existing image', command=self.use_existing_image)
+        menubar.add_command(label='Use existing image',
+                            command=self.use_existing_image)
         menubar.add_command(label='Exit', command=self.close_program)
         self.root.config(menu=menubar)
         # Labels
-        self.folder_label = ttk.Label(self.mainframe, text='Choose folder:', font=16)
-        self.listbox_label = ttk.Label(self.mainframe, text='List of secured folder:', font=16)
+        self.folder_label = ttk.Label(
+            self.mainframe, text='Choose folder:', font=16)
+        self.listbox_label = ttk.Label(
+            self.mainframe, text='List of secured folder:', font=16)
         # Entries
-        self.folder_entry = ttk.Entry(self.mainframe, width=90, text='', background='white')
+        self.folder_entry = ttk.Entry(
+            self.mainframe, width=90, text='', background='white')
         # List box
-        self.folders_listbox = Listbox(self.mainframe, width=90, background='white')
-
+        self.folders_listbox = Listbox(
+            self.mainframe, width=90, background='white')
+        self.data = get_paths_from_dir()
+        for path in self.data:
+            self.folders_listbox.insert(1, path.strip('\n'))
         # Buttons
-        self.browse = ttk.Button(self.mainframe, text='Browse...', command=self.get_folder_path)
-        self.lock_btn = ttk.Button(self.mainframe, text='Lock', command=self.lock_directory)
-        self.unlock_btn = ttk.Button(self.mainframe, text='Unlock', command=self.unlock_dir)
+        self.browse = ttk.Button(
+            self.mainframe, text='Browse...', command=self.get_folder_path)
+        self.lock_btn = ttk.Button(
+            self.mainframe, text='Lock', command=self.lock_directory)
+        self.unlock_btn = ttk.Button(
+            self.mainframe, text='Unlock', command=self.unlock_dir)
         # Grigds
         # folder grid
         self.folder_label.grid(column=1, row=1)
@@ -59,37 +84,35 @@ class UI:
         # list box grid
         self.listbox_label.grid(column=1, row=2)
         self.folders_listbox.grid(column=2, row=3)
-        
+
     def close_program(self):
         self.root.destroy()
 
     def get_folder_path(self):
         user_desk = '/home/{}/Desktop'.format(getuser())
-        self.browse = filedialog.askdirectory(initialdir=user_desk, title='Select folder')
+        self.browse = filedialog.askdirectory(
+            initialdir=user_desk, title='Select folder')
         self.folder_entry.delete(0, 'end')
         self.folder_entry.insert(0, self.browse)
 
-    def write_path_to_file(self):
-        path_to_save = os.path.join(os.getcwd(), 'paths.txt')
-        with open(path_to_save, 'w') as f:
-            f.write(self.browse)
-
-    def remove_path_from_file(self):
-        with open('paths.txt', 'r+') as f:
-            paths = f.readlines()
-            if self.browse in paths:
-                paths.remove(self.browse)
-            f.write(paths)
+    def is_secured(self):
+        active = self.browse
+        with open('paths.txt') as f:
+            data = f.readlines()
+        for path in data:
+            if active in path:
+                return True
+        return False
 
     def lock_directory(self):
-        if self.browse in self.set_of_paths:
-            messagebox.showinfo(message='Already in list')
+        active_path = self.browse
+        if self.is_secured():
+            messagebox.showinfo(message='Already secured')
         else:
-            self.folders_listbox.insert(1, self.browse)
+            self.folders_listbox.insert(0, active_path)
             messagebox.showinfo(message='Folder is locked')
-            call(['chmod', '-R', '101', self.folders_listbox.get('active')])
-            self.set_of_paths.add(self.browse)
-            self.write_path_to_file()
+            call(['chmod', '-R', '101', active_path])
+            write_path_to_file(active_path)
 
     def use_existing_image(self):
         path = os.getcwd
@@ -100,6 +123,8 @@ class UI:
         self.path_to_image.append(image_path)
 
     def unlock_dir(self):
+        active_item = self.folders_listbox.get('active')
+        print(active_item)
         if len(self.path_to_image) == 0:
             try:
                 access = find_and_recognize()
@@ -107,10 +132,8 @@ class UI:
                 messagebox.showinfo(message=err)
             else:
                 if access:
-                    call(['chmod', '-R', '777', self.folders_listbox.get('active')])
-                    self.folders_listbox.delete(0)
-                    self.set_of_paths.discard(self.browse)
-                    self.remove_path_from_file()
+                    call(['chmod', '-R', '777', active_item])
+                    self.folders_listbox.delete('active')
                     messagebox.showinfo(message='Access granted.')
                 else:
                     messagebox.showinfo(message='Access denied.')
@@ -121,14 +144,14 @@ class UI:
                 messagebox.showinfo(message=err)
             else:
                 if access:
-                    call(['chmod', '-R', '777', self.folders_listbox.get('active')])
-                    self.folders_listbox.delete(0)
-                    self.set_of_paths.discard(self.browse)
-                    self.remove_path_from_file()
+                    call(['chmod', '-R', '777', active_item])
+                    self.folders_listbox.delete('active')
                     messagebox.showinfo(message='Access granted.')
                 else:
                     messagebox.showinfo(message='Access denied.')
+        remove_paths(active_item)
 
-print('Start programm...')
-main = UI()
-main.root.mainloop()
+if __name__ == '__main__':
+    print('Start programm...')
+    main = UI()
+    main.root.mainloop()
